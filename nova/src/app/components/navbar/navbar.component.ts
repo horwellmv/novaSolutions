656c496b2dd625a9a20059d74417e4c3fab65a6c
Horwell <horwellmv@gmail.com>
2024-04-04 03:13:27 -0300

    Crea nuevo componente Home

--- nova/src/app/components/navbar/navbar.component.ts
+++ nova/src/app/components/navbar/navbar.component.ts
@@ -8,11 +8,17 @@ import { Component } from '@angular/core';
 export class NavbarComponent {
   inOutMenu() {
     const menu = document.querySelector("#menu");
+    //selectores ID para ocultar los componentes al mostrar el Menu
+    //Agregando la clase Hidden - hacer con cada componente.
     const home = document.querySelector("#home");
+    const services = document.querySelector("#services");
+
     const icono = document.querySelector(".fa-solid");
 
     menu?.classList.toggle("hidden");
-    home?.classList.toggle("hidden")
+    home?.classList.toggle("hidden");
+    services?.classList.toggle("hidden");
+
     icono?.classList.toggle("fa-xmark");
   }
 }
